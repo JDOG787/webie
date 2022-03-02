5fda4ed81de2cffdce754c0edea9875182fa9540
JDOG787 <64325143+JDOG787@users.noreply.github.com>
2022-03-01 20:01:10 -0700

    basic route stuff

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+const server = require('./lib/index.js');
+const web = server(8080);
+
+
+web.get('/', (req, res) => {
+    res.write("<h1>root!</h1>");
+    res.end();
+});
+
+web.get('/hello', (req, res) => {
+    res.write("<h1>Hello!</h1>");
+    res.end();
+});
